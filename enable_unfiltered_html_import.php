a5727186b76f703424f7054dd680c4bffd3ad8be
Bill Pickard <bill.r.pickard@gmail.com>
2017-07-20 13:55:47 -0700

    Update enable_unfiltered_html_import.php

--- enable_unfiltered_html_import.php
+++ enable_unfiltered_html_import.php
@@ -1,7 +1,7 @@
 /**
  * Plugin Name: Enable Unfiltered HTML on Import
  * Plugin URI: https://github.com/bpickard/Web-Development/blob/master/enable_unfiltered_html_import.php
- * Description: This plugin allows unfiltered HTML such as <iframe> and <script> tags to be imported
+ * Description: This plugin allows unfiltered HTML such as <iframe> and <script> tags to be imported into a Wordpress Multisite
  * Version: 1.0.0
  * Author: Bill Pickard
  * Author URI: 
